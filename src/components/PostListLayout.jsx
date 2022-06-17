00688e4a034275be5c5b53544f8473853a970672
Andreas <andrew.eracleous@gmail.com>
2022-06-17 12:18:07 +0300

    CHANGED: updated query for post list & single post for images

--- src/components/PostListLayout.jsx
+++ src/components/PostListLayout.jsx
@@ -26,33 +26,31 @@ const PostListLayout = ({ data }) => {
 	const postCard = memoizedData[selectedDataIndex.current].map(
 		({ id, frontmatter, slug }, index) => (
 			<article
-				className="relative group cursor-pointer rounded-xl shadow bg-white transition ease-in-out hover:-translate-y-1 hover:scale-105	duration-200"
+				className="relative flex cursor-pointer flex-col rounded-xl bg-white shadow transition ease-in-out hover:scale-105"
 				key={id}
 			>
-				<Link className="block mx-auto w-full h-56" to={`/${slug}`}>
-					<Image
-						className="rounded-t-xl max-w-full h-56"
-						filename={frontmatter?.featuredImage}
-						title={frontmatter?.title}
-						withFallback={!frontmatter?.featuredImage}
-					/>
+				<Link to={`/${slug}`} className="h-full text-slate-800 hover:underline">
+					<div>
+						<div className="mx-auto block h-56 w-full">
+							<Image
+								className="h-56 max-w-full rounded-t-xl"
+								filename={frontmatter?.featuredImage}
+								title={frontmatter?.title}
+							/>
+						</div>
+						<header className="px-3 py-3 md:px-5">
+							<time
+								className="text-base font-light text-slate-500"
+								dateTime={frontmatter?.datetime}
+							>
+								{frontmatter?.date}
+							</time>
+							<h3 className="m-0 text-xl font-semibold dark:text-slate-800">
+								{frontmatter?.title}
+							</h3>
+						</header>
+					</div>
 				</Link>
-				<header className="px-3 md:px-5 py-3">
-					<time
-						className="text-slate-500 font-light text-base"
-						dateTime={frontmatter?.datetime}
-					>
-						{frontmatter?.date}
-					</time>
-					<h3 className="text-xl font-semibold m-0">
-						<Link
-							className="block group-hover:underline text-slate-800"
-							to={`/${slug}`}
-						>
-							{frontmatter?.title}
-						</Link>
-					</h3>
-				</header>
 			</article>
 		),
 	)
@@ -63,7 +61,7 @@ const PostListLayout = ({ data }) => {
 
 	return (
 		<>
-			<section className="mt-16 grid gap-5 md:gap-8 xl:gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+			<section className="mt-16 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10">
 				{postCard}
 			</section>
 			<Pagination
--- src/templates/BlogPost.jsx
+++ src/templates/BlogPost.jsx
@@ -1,69 +1,74 @@
-import { graphql } from "gatsby"
-import React from "react"
-import Seo from "../components/Seo"
-import PostLayout from "../components/PostLayout"
+import { graphql } from 'gatsby'
+import React from 'react'
+import Seo from '../components/Seo'
+import PostLayout from '../components/PostLayout'
 
 const BlogPost = ({ data, pageContext }) => {
-  const {
-    id,
-    body,
-    excerpt,
-    frontmatter: { featuredImage, title, date, datetime },
-  } = data.mdx
+	const {
+		id,
+		body,
+		excerpt,
+		frontmatter: { featuredImage, title, date, datetime },
+	} = data.mdx
 
-  return (
-    <>
-      <Seo title={title} description={excerpt} />
-      <hr className="border-gray-200 mb-8" />
-      <PostLayout
-        featuredImage={featuredImage}
-        title={title}
-        content={body}
-        date={date}
-        datetime={datetime}
-        previous={data.previous}
-        next={data.next}
-        key={id}
-      />
-    </>
-  )
+	return (
+		<>
+			<Seo title={title} description={excerpt} />
+			<hr className="border-gray-200 mb-8" />
+			<PostLayout
+				featuredImage={featuredImage}
+				title={title}
+				content={body}
+				date={date}
+				datetime={datetime}
+				previous={data.previous}
+				next={data.next}
+				key={id}
+			/>
+		</>
+	)
 }
 
 export default BlogPost
 
 export const query = graphql`
-  query BLOG_POST_BY_SLUG(
-    $id: String!
-    $previousPostId: String
-    $nextPostId: String
-  ) {
-    mdx(id: { eq: $id }) {
-      id
-      slug
-      body
-      excerpt
-      frontmatter {
-        datetime: date
-        date(formatString: "DD MMMM YYYY")
-        title
-        featuredImage {
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
-      }
-    }
-    previous: mdx(id: { eq: $previousPostId }) {
-      slug
-      frontmatter {
-        title
-      }
-    }
-    next: mdx(id: { eq: $nextPostId }) {
-      slug
-      frontmatter {
-        title
-      }
-    }
-  }
+	query BLOG_POST_BY_SLUG(
+		$id: String!
+		$previousPostId: String
+		$nextPostId: String
+	) {
+		mdx(id: { eq: $id }) {
+			id
+			slug
+			body
+			excerpt
+			frontmatter {
+				datetime: date
+				date(formatString: "DD MMMM YYYY")
+				title
+				featuredImage {
+					childImageSharp {
+						gatsbyImageData(
+							width: 500
+							transformOptions: { cropFocus: CENTER }
+							blurredOptions: { width: 100 }
+							placeholder: BLURRED
+						)
+					}
+				}
+			}
+		}
+		previous: mdx(id: { eq: $previousPostId }) {
+			slug
+			frontmatter {
+				title
+			}
+		}
+		next: mdx(id: { eq: $nextPostId }) {
+			slug
+			frontmatter {
+				title
+			}
+		}
+	}
 `
